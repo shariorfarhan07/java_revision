bbf36d4f4e9a26c6eb2a354f526d4348af46988b
shariorfarhan07 <md.sharior.hossain.farhan@g.bracu.ac.bd>
2022-11-15 04:15:42 +0600

    test

--- DataStructure/hashSetTest.java
+++ DataStructure/hashSetTest.java
@@ -5,13 +5,19 @@ import java.util.HashSet;
 public class hashSetTest {
     public static void main(String[] args) {
         HashSet<Integer> h=new HashSet<>();
-        h.add(1);
-        h.add(2);
-        h.add(3);
         h.add(4);
         h.add(5);
         h.add(5);
+        h.add(1);
+        h.add(2);
+        h.add(3);
+
         System.out.println(h);
+        hashSetTest t;
+        t.lala();
     }
 
+    public static void lala() {
+        System.out.println("farhan");
+    }
 }
