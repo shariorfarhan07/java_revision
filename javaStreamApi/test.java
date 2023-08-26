85c09ed209cd3db4313e097cd63af10f48bf062e
shariorfarhan07 <shariorfarhan77@gmail.com>
2023-08-26 15:18:40 +0600

    update

--- javaStreamApi/test.java
+++ javaStreamApi/test.java
@@ -2,10 +2,13 @@ package javaStreamApi;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class test {
     public static void main(String[] args) {
         List<Integer> ls= Arrays.asList(1,2,3,4,5,6,7,8,9);
-        ls.stream()
-    }
+        Stream<Integer> data= ls.stream();
+//        data.forEach(n-> System.out.println(n));
+        data.forEach(n-> System.out.println(n*n));
+    } 
 }
